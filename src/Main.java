dfecdba00864640124ba2b976405d88bfd5587f0
Aleksandr Polikarpov <nub.and.co@gmail.com>
2023-11-17 20:06:31 +0300

    Bonus

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-               
+        int balance = 250; // сумма присутствуящая на счету
+        int replenishment = 1325; // сумма пополнения
+        int bonusPrice = 100; // цена бонусного рубля
+        int bonusCash;
+
+        if (replenishment > 1000) {
+            bonusCash = replenishment / bonusPrice;
+        } else {
+            bonusCash = 0;
         }
+        int newBalance = balance + replenishment +bonusCash;
+        System.out.println("На вашем счету - " + newBalance + "руб.; бонус составил - " + bonusCash + "руб.");
     }
 }
